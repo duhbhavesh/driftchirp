e537ac6af080795187909f2cd6a0e4fedca8276a
Bhavesh Kasturi <connect.bhavesh.kasturi@gmail.com>
2021-06-09 17:21:43 +0530

    feat: add custom colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,24 @@ module.exports = {
    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
    darkMode: false, // or 'media' or 'class'
    theme: {
-      extend: {},
+      extend: {
+         colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            blue: {
+               light: '#4A72E8',
+               DEFAULT: '#2453E2',
+               dark: '#1C4BD9',
+            },
+            black: {
+               darkest: '#0B0E11',
+               dark: '#151A21',
+               DEFAULT: '#1B2129',
+               light: '#202731',
+               lightest: '#303B4A',
+            },
+         },
+      },
    },
    variants: {
       extend: {},
