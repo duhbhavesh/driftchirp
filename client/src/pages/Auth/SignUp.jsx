e2805fcd74f5bb931059ea86b857249ba0f541f7
Bhavesh Kasturi <connect.bhavesh.kasturi@gmail.com>
2021-06-15 17:51:06 +0530

    feat: add Login, Signup page

--- client/src/pages/Auth/SignUp.jsx
+++ client/src/pages/Auth/SignUp.jsx
@@ -2,8 +2,39 @@ import React from 'react'
 
 export default function SignUp() {
   return (
-    <div>
-      <h1>SignUp</h1>
+    <>
+    <div className="flex max-w-sm mx-auto overflow-hidden bg-white rounded-xl shadow-lg lg:max-w-4xl mt-10">
+        <div className="hidden bg-cover lg:block lg:w-1/2" style={{backgroundImage: "url('https://images.unsplash.com/photo-1616960371261-12f51dccb852?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80')"}}></div>
+        <div className="w-full px-6 py-8 md:px-8 lg:w-1/2 pt-10 pb-14">
+            <h2 className="text-2xl font-bold text-center text-black mb-8">Sign Up</h2>
+            <div className="mt-4">
+                <label className="block mb-2 text-sm font-medium text-black-light" for="input-first-name">First Name</label>
+                <input id="input-first-name" placeholder="John" className="block w-full px-4 py-2 text-black-lightest bg-white border border-gray-300 rounded-md focus:border-blue-light focus:outline-none focus:ring-2" type="text" />
+            </div>
+            <div className="mt-4">
+                <label className="block mb-2 text-sm font-medium text-black-light" for="input-last-name">Last Name</label>
+                <input id="input-last-name" placeholder="Smith" className="block w-full px-4 py-2 text-black-lightest bg-white border border-gray-300 rounded-md focus:border-blue-light focus:outline-none focus:ring-2" type="text" />
+            </div>
+            <div className="mt-4">
+                <label className="block mb-2 text-sm font-medium text-black-light" for="input-email">Email Address</label>
+                <input id="input-email" placeholder="name@example.com" className="block w-full px-4 py-2 text-black-lightest bg-white border border-gray-300 rounded-md focus:border-blue-light focus:outline-none focus:ring-2" type="email" />
+            </div>
+            <div className="mt-4">
+                <label className="block mb-2 text-sm font-medium text-black-light" for="input-password">Password</label>
+                <input id="input-password" placeholder="password" className="block w-full px-4 py-2 text-black-lightest bg-white border border-gray-300 rounded-md focus:border-blue-light focus:outline-none focus:ring-2" type="password" />
+            </div>
+            <div className="mt-8">
+                <button className="w-full px-4 py-3 tracking-wide font-bold text-white transition-colors duration-200 transform bg-blue rounded-full hover:bg-blue-light focus:outline-none focus:bg-blue-dark">
+                    Sign Up!
+                </button>
+            </div>
+            <div className="flex items-center justify-between mt-4">
+                <span className="w-1/5 border-b md:w-1/4"></span>
+                <a href="/" className="text-sm text-black hover:underline">Existing User? Login</a>
+                <span className="w-1/5 border-b md:w-1/4"></span>
+            </div>
+        </div>
     </div>
+    </>
   )
 }
